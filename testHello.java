4b00071934eff32382024e5a19a768c12499db2b
luckysers <luckyparmar.sers@gmail.com>
2025-06-11 20:30:11 +0530

    jenkins scm test

--- testHello.java
+++ testHello.java
@@ -1,5 +1,5 @@
 public class testHello{
     public static void main(String[] args){
-        System.out.println("hello world.. First Jenkins Job");
+        System.out.println("hello world.. First Jenkins Job with SCM");
     }
 }
